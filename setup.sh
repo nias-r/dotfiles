9bcedce15242d387de043b959e7135b45492692f
Richard Nias <nias@wirewax.com>
2017-08-23 16:55:18 +0100

    first pass at a setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# antigen
+brew install antigen
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
