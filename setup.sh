07f02b5d8dfd707319a45c2f112a419f7b905558
Richard Nias <richardnias@gmail.com>
2018-10-16 12:41:04 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 curl -L git.io/antigen > ~/.antigen.zsh
 
 # Vundle
-git clone git@github.com:/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[[ -d ~/.vim/bundle/Vundle.vim ]] || git clone git@github.com:/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # tmux
-cd
-git clone git@github.com:gpakosz/.tmux.git 
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+[[ -d ~/.tmux ]] || git clone git@github.com:gpakosz/.tmux.git ~/.tmux
+ln -sf ~/.tmux/.tmux.conf ~/.tmux.conf
+
+# link
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf.local ~/.tmux.conf.local
