0ab2e58472e4238af47f90250c2bb754c328e808
Richard Nias <richardnias@gmail.com>
2018-10-28 19:16:35 +0100

    fix gdate

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+alias gdate=/usr/local/bin/gdate
 echo "`TZ=UTC gdate -Iseconds`: Beginning backup"
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Photo\ Library/ s3://richardnias/photos/ --storage-class STANDARD_IA
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Movies/sofa/ s3://richardnias/sofa/ --storage-class STANDARD_IA
