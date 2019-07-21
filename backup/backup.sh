ad9bc6c391f7622e8072acb7100944701e18548d
Richard Nias <richardnias@gmail.com>
2019-07-21 13:46:13 +0200

    improve date logic in backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-alias gdate=/usr/local/bin/gdate
-echo "`TZ=UTC gdate -Iseconds`: Beginning backup"
+
+function logdate() {
+		/usr/local/bin/gdate $* 2> /dev/null || date
+}
+
+echo "`TZ=UTC logdate -Iseconds`: Beginning backup"
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Photo\ Library/ s3://richardnias/photos/ --storage-class STANDARD_IA
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Movies/sofa/ s3://richardnias/sofa/ --storage-class STANDARD_IA
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Lightroom/ s3://richardnias/lightroom/ --storage-class STANDARD_IA
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Music/iTunes/iTunes\ Media/Music/ s3://richardnias/music/ --storage-class STANDARD_IA
-echo "`TZ=UTC gdate -Iseconds`: Finished backup"
+echo "`TZ=UTC logdate -Iseconds`: Finished backup"
