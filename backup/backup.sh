ceab6972164ab66c44674998b413dabce894e7cc
Richard Nias <richardnias@gmail.com>
2020-09-04 17:23:46 +0200

    better date logging

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-trap 'echo "FAILED!"' ERR
+trap 'echo "FAILED!" | ts ${DATE_FORMAT}' ERR
 
-function logdate() {
-		/usr/local/bin/gdate $* 2> /dev/null || date
-}
+TZ=UTC
+DATE_FORMAT="%Y-%m-%dT%H:%M:%S%z"
 
-echo "`TZ=UTC logdate -Iseconds`: Beginning backup"
-/Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Photo\ Library/ s3://richardnias/photos/ --storage-class STANDARD_IA
-/Users/nias/.local/bin/aws s3 sync /Users/nias/Movies/sofa/ s3://richardnias/sofa/ --storage-class STANDARD_IA
-/Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Lightroom/ s3://richardnias/lightroom/ --storage-class STANDARD_IA
-/Users/nias/.local/bin/aws s3 sync /Users/nias/Music/iTunes/iTunes\ Media/Music/ s3://richardnias/music/ --storage-class STANDARD_IA
-/Users/nias/.local/bin/aws s3 sync /Users/nias/Music/Ableton/User\ Library/ s3://richardnias/ableton-user-library/ --storage-class STANDARD_IA
-/Users/nias/.local/bin/aws s3 sync /Users/nias/projects/ s3://richardnias/ableton-projects/ --storage-class STANDARD_IA
-/Users/nias/.local/bin/aws s3 sync /Users/nias/sounds/ s3://richardnias/sounds/ --storage-class STANDARD_IA
-echo "`TZ=UTC logdate -Iseconds`: Finished backup"
+echo "Beginning backup" | ts ${DATE_FORMAT}
+/Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Photo\ Library/ s3://richardnias/photos/ --storage-class STANDARD_IA --no-progress | ts ${DATE_FORMAT}
+/Users/nias/.local/bin/aws s3 sync /Users/nias/Movies/sofa/ s3://richardnias/sofa/ --storage-class STANDARD_IA --no-progress | ts ${DATE_FORMAT}
+/Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Lightroom/ s3://richardnias/lightroom/ --storage-class STANDARD_IA --no-progress | ts ${DATE_FORMAT}
+/Users/nias/.local/bin/aws s3 sync /Users/nias/Music/iTunes/iTunes\ Media/Music/ s3://richardnias/music/ --storage-class STANDARD_IA --no-progress | ts ${DATE_FORMAT}
+/Users/nias/.local/bin/aws s3 sync /Users/nias/Music/Ableton/User\ Library/ s3://richardnias/ableton-user-library/ --storage-class STANDARD_IA --no-progress | ts ${DATE_FORMAT}
+/Users/nias/.local/bin/aws s3 sync /Users/nias/projects/ s3://richardnias/ableton-projects/ --storage-class STANDARD_IA --no-progress | ts ${DATE_FORMAT}
+/Users/nias/.local/bin/aws s3 sync /Users/nias/sounds/ s3://richardnias/sounds/ --storage-class STANDARD_IA --no-progress | ts ${DATE_FORMAT}
+echo "Finished backup" | ts ${DATE_FORMAT}
