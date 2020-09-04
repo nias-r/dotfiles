d55f46b98f43d6ba8dc8cbeb16e1b1b308d6ceab
Richard Nias <richardnias@gmail.com>
2020-09-04 14:37:53 +0200

    Backup more stuff

--- backup/backup.sh
+++ backup/backup.sh
@@ -12,4 +12,7 @@ echo "`TZ=UTC logdate -Iseconds`: Beginning backup"
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Movies/sofa/ s3://richardnias/sofa/ --storage-class STANDARD_IA
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Pictures/Lightroom/ s3://richardnias/lightroom/ --storage-class STANDARD_IA
 /Users/nias/.local/bin/aws s3 sync /Users/nias/Music/iTunes/iTunes\ Media/Music/ s3://richardnias/music/ --storage-class STANDARD_IA
+/Users/nias/.local/bin/aws s3 sync /Users/nias/Music/Ableton/User\ Library/ s3://richardnias/ableton-user-library/ --storage-class STANDARD_IA
+/Users/nias/.local/bin/aws s3 sync /Users/nias/projects/ s3://richardnias/ableton-projects/ --storage-class STANDARD_IA
+/Users/nias/.local/bin/aws s3 sync /Users/nias/sounds/ s3://richardnias/sounds/ --storage-class STANDARD_IA
 echo "`TZ=UTC logdate -Iseconds`: Finished backup"
